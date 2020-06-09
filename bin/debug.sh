58e85c3803a278ff1603866ebb4c3e083e52fdc2
cjmatta <chris@confluent.io>
2020-06-09 09:48:58 -0400

    improved debug scripts, including HTML 5 logo

--- bin/debug.sh
+++ bin/debug.sh
@@ -1,11 +1,21 @@
 
 #!/usr/bin/env bash
-
+DIR=$(cd $(dirname $0) && pwd)
 : ${SUSPEND:='n'}
 
 set -e
+BUILD=false
+while getopts b option; do
+    case "${option}"
+    in
+        b) BUILD=true;;
+    esac
+done
 
-mvn clean package
+if [[ ${BUILD} = true ]]; then
+    mvn clean package
+fi
+
 export KAFKA_JMX_OPTS="-Xdebug -agentlib:jdwp=transport=dt_socket,server=y,suspend=${SUSPEND},address=5005"
 
-connect-standalone config/connect-avro-docker.properties config/MySinkConnector.properties config/MySourceConnector.properties
+connect-standalone ${DIR}/../config/connect-avro-docker.properties ${DIR}/../config/kafka-connect-sse.properties
